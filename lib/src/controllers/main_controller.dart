1ef4fdb5262d3a88ae94beee73c9d89c765ddd53
bunpanin <bunpanin017@gmail.com>
2026-01-28 20:22:34 +0700

    features: count number

---
+++ lib/src/controllers/main_controller.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+class MainController extends GetxController{
+  var number = 0.obs;
+  var myString = "".obs;
+  var myList = [].obs;
+
+
+  void addNumber(){
+    number.value += 1;
+  }
+
+  void minusNumber(){
+    number.value -=1;
+  }
+}
--- lib/src/pages/main_pages.dart
+++ lib/src/pages/main_pages.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import '../controllers/main_controller.dart';
 
 class MainPages extends StatelessWidget {
-  const MainPages({super.key});
+  // final controller = Get.put(MainController());
+  MainPages({super.key});
+  var controller = Get.put(MainController());
+
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.amber,
-      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Obx(()=>
+              Text("Count Number : ${controller.number}"),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: (){
+              HapticFeedback.lightImpact();
+              controller.addNumber();
+            }, child: Text("Click here to Add number",
+            )),
+            ElevatedButton(
+              onPressed: (){
+              HapticFeedback.lightImpact();
+              controller.minusNumber();
+            }, child: Text("Click here to Minus number",
+            )),
+          ],
+        ),
+      )
     );
   }
 }
