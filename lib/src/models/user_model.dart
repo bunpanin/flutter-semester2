aaceee74385889727f600e375f33114e1b8c35f0
bunpanin <bunpanin017@gmail.com>
2026-02-04 19:56:52 +0700

    features: complete user model

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -14,4 +14,26 @@ class UserModel {
     this.phone,
     this.accessToken,
   });
+
+  // From Json
+  UserModel.fromJson(Map<String,dynamic>json){
+    id=json['id'];
+    name=json['name'];
+    id=json['email'];
+    id=json['password'];
+    id=json['phone'];
+    id=json['accessToken'];
+  }
+
+  // To Json
+  Map<String,dynamic> toJson()=>{
+    'id':id,
+    'name':name,
+    'email':email,
+    'password':password,
+    'phone':phone,
+    'accessToken':accessToken,
+  };
+
+
 }
